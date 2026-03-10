878b65afd1b427d72f8bbb9099fe7141db510206
diya.r <dram0027@student.monash.edu>
2026-03-11 10:11:08 +1100

    added documentation for valid_sudoku

--- arrays_and_hashing/valid_sudoku.py
+++ arrays_and_hashing/valid_sudoku.py
@@ -10,32 +10,42 @@ Author: Diya Ramesh
 Time: O(n^2)
     - where n = #rows
     - required to check all elements in the board
-Space:
+Space: O(n)
+    - where n = #rows
+    - since sets are being re-used for each iteration
+
+Note: O(1) for time and space since it is always a 9x9 grid
 """
 
 def isValidSudoku(board: List[List[str]]) -> bool:        
     for i, row in enumerate(board):
-        row_set, col_set = set(), set()
+
+        row_freq, col_freq = set(), set()
 
         for j, cell in enumerate(row):
+
             # check row
-            if row[j] != "." and row[j] in row_set:
+            if row[j] != "." and row[j] in row_freq: # O(1) search in hash-set
                 return False
-            row_set.add(cell)
+            row_freq.add(cell)
 
             # check column
-            if board[j][i] != "." and board[j][i] in col_set:
+            if board[j][i] != "." and board[j][i] in col_freq:
                 return False
-            col_set.add(board[j][i])
+            col_freq.add(board[j][i])
         
     # check sub-box 
-    for x in range(0, 7, 3):
-        for y in range(0, 7, 3):
-            freq = set()
-            for row in board[x:x+3]: 
-                for z in range(y, y+3): 
-                    if row[z] != "." and row[z] in freq:
+    for x in range(0, 7, 3): # 0 -> 3 -> 6
+        for y in range(0, 7, 3): # 0 -> 3 -> 6
+            sub_box_freq = set()
+
+            # looks at 3 rows
+            for row in board[x:x+3]: # 0:3 -> 3:6 -> 6:9
+
+                # gets sub-boxes from L-R
+                for z in range(y, y+3):  # 0:3 -> 3:6 -> 6:9
+                    if row[z] != "." and row[z] in sub_box_freq:
                         return False
-                    freq.add(row[z])
+                    sub_box_freq.add(row[z])
         
     return True
